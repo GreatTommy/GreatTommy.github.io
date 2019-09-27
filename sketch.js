3e08e855f0b2b942d1ce4ff1d45bb54384d3d093
GreatTommy <tlaurent21@gmail.com>
2019-09-27 19:34:12 +0200

    update

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 //https://fr.wikipedia.org/wiki/Algorithme_de_Prim
-/*L'algorithme7 consiste à faire croître un arbre depuis un sommet. On commence avec un seul
+/*L'algorithme consiste à faire croître un arbre depuis un sommet. On commence avec un seul
 sommet puis à chaque étape, on ajoute une arête de poids minimum ayant exactement une extrémité
 dans l'arbre en cours de construction. En effet, si ses deux extrémités appartenaient déjà à
 l'arbre, l'ajout de cette arête créerait un deuxième chemin entre les deux sommets dans l'arbre
@@ -10,22 +10,23 @@ let pointsRelies = [];
 
 function setup() {
   createCanvas(600, 600);
+  reset();
 }
 
 function draw() {
-  background(200);
   if (pointsNonRelies.length > 0) {
     relier();
   }
   for (let elt of pointsRelies) {
-    elt.afficher();
-    elt.afficherLien();
+    stroke(0);
+    strokeWeight(2);
+    circle(elt.x, elt.y, 15);
   }
 }
 
 function mousePressed() {
   reset();
-  pt = new Point(mouseX, mouseY);
+  pt = createVector(mouseX, mouseY);
   pointsNonRelies.push(pt);
 }
 
@@ -35,46 +36,31 @@ function relier() {
     pointsRelies.push(pointsNonRelies[n]);
     pointsNonRelies.splice(n, 1);
   }
-  let min = height;
-  let indiceI = -1;
-  let indiceJ = -1;
-  let distance;
-  for (let i = 0; i < pointsRelies.length; i++) {
-    for (let j = 0; j < pointsNonRelies.length; j++) {
-      distance = dist(pointsRelies[i].x, pointsRelies[i].y, pointsNonRelies[j].x, pointsNonRelies[j].y);
-      if (distance < min) {
-        min = distance;
-        indiceI = i;
-        indiceJ = j;
+  while (pointsNonRelies.length > 0) {
+    let min = height;
+    let indiceI;
+    let indiceJ;
+    let distance;
+    for (let i = 0; i < pointsRelies.length; i++) {
+      for (let j = 0; j < pointsNonRelies.length; j++) {
+        distance = dist(pointsRelies[i].x, pointsRelies[i].y, pointsNonRelies[j].x, pointsNonRelies[j].y);
+        if (distance < min) {
+          min = distance;
+          indiceI = i;
+          indiceJ = j;
+        }
       }
     }
-  }
-  if (indiceI != -1 && indiceJ != -1) {
-    pointsNonRelies[indiceJ].indiceVoisin = indiceI;
+    stroke(255, 0, 0);
+    strokeWeight(3);
+    line(pointsRelies[indiceI].x, pointsRelies[indiceI].y, pointsNonRelies[indiceJ].x, pointsNonRelies[indiceJ].y);
     pointsRelies.push(pointsNonRelies[indiceJ]);
     pointsNonRelies.splice(indiceJ, 1);
   }
 }
 
 function reset() {
+  background(200);
   pointsNonRelies = pointsRelies;
   pointsRelies = [];
-}
-
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.indiceVoisin = 0;
-  }
-  afficher() {
-    stroke(0);
-    strokeWeight(2);
-    circle(this.x, this.y, 15);
-  }
-  afficherLien() {
-    stroke(255, 0, 0);
-    strokeWeight(3);
-    line(this.x, this.y, pointsRelies[this.indiceVoisin].x, pointsRelies[this.indiceVoisin].y);
-  }
 }
